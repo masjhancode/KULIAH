6b4b0e86af4c70bf65f5382f608d16bd075e91aa
Shafiyya Mahira Ulinnuha <via@Shafiyyas-MacBook-Air.local>
2025-08-19 12:19:24 +0900

    remove folder kelasif1

--- kelasif1/bin/kelasif1.dart
+++
@@ -1,5 +0,0 @@
-import 'package:kelasif1/kelasif1.dart' as kelasif1;
-
-void main(List<String> arguments) {
-  print('Hello world: ${kelasif1.calculate()}!');
-}
--- kelasif1/test/kelasif1_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:kelasif1/kelasif1.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
